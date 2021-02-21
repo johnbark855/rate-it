0b1955f34de3b387905d99b909d8ed7e13429722
lucpizz <lucpizz@gmail.com>
2021-02-21 16:16:24 -0500

    added handlebars code to display rows

--- server.js
+++ server.js
@@ -19,9 +19,8 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
-
-
 
 db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
