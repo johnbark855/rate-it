9208ee8004def12f6d4ac950b9feaeff9c32c508
lucpizz <lucpizz@gmail.com>
2021-02-21 16:17:26 -0500

    add create and delete code

---
+++ routes/api-routes.js
@@ -0,0 +1,22 @@
+const db = require("../models");
+
+module.exports = (app) => {
+  app.get("/api/items", function (req, res) {
+    db.Items.findAll({}).then((dbPost) => res.json(dbPost));
+  });
+
+  app.post("/api/items", function (req, res) {
+    db.Items.create({
+      item: req.body.userInput,
+      rating: req.body.userRating,
+    }).then((dbPost) => res.json(dbPost));
+  });
+
+  app.delete("/api/items/:id", function (req, res) {
+    db.Items.destroy({
+      where: {
+        id: req.params.id,
+      },
+    }).then((dbPost) => res.json(dbPost));
+  });
+};
