f2fd9884fbb6cb6aa03c40bffe056cee7ad4c8a0
lucpizz <lucpizz@gmail.com>
2021-02-22 12:09:25 -0500

    update and fixed delete feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,34 @@
+/*
+  Programs main functionality using JQuery to adding and deleting user input for restaurants and movies
+*/
+
 $(document).ready(function () {
   $(".sidenav").sidenav();
+
+  // variables to get on-click responses from user interaction
 
   let formInput = $("form.formInput");
   let userInput = $("input#userInput");
   let userRating = $("input#userRating");
-  let userDelete = $("input#userDelete");
+  let userDelete = $(".userDelete");
+
+  // addRating does a post to our "api/item" route and if successful, redirects us the the main page
+
+  function addRating(Input, Rating) {
+    console.log("test");
+    $.post("/api/items", {
+      userInput: Input,
+      userRating: Rating,
+    })
+      .then(function () {
+        window.location.replace("/");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
+  // Adds user input from form upon the submit click event with button
 
   formInput.on("submit", function (event) {
     event.preventDefault();
@@ -20,32 +44,17 @@ $(document).ready(function () {
     userInput.val("");
     userRating.val("");
   });
-  // addRating does a post to our "api/item" route and if successful, redirects us the the main page
-  function addRating(Input, Rating) {
-    // console.log(userInput, userRating);
-    // console.log(typeof userInput, typeof userRating);
-    console.log("test");
-    $.post("/api/items", {
-      userInput: Input,
-      userRating: Rating,
-    })
-      .then(function () {
-        window.location.replace("/");
-        // If there's an error, log the error
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  }
 
-  $(".userDelete").on("click", function (e) {
-    let id = `${e.target.id}`;
+  // Deletes the user entries upon the click event with trashcan icon
+
+  userDelete.on("click", function (event) {
+    let id = `${event.target.id}`;
     console.log(id);
     fetch(`/api/items/${id}/`, {
       method: "DELETE",
     }).then(function (response) {
       if (response.ok) {
-        location.replace(`/`);
+        location.replace("/");
       }
     });
   });
